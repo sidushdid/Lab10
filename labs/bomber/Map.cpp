756af51c5763f491f4526e2507eaa533c483fd10
Jackson Cao <jacksoncao@ucsb.edu>
2025-03-10 18:58:46 -0700

    Fixing the rest of map and add makefile

--- labs/bomber/Map.cpp
+++ labs/bomber/Map.cpp
@@ -2,18 +2,27 @@
 
 // Member Functions
 Map::Map(std::istream& stream){
-    std::istream temp_stream = stream;
+    y_size = std::count(std::istreambuf_iterator<char>(stream), std::istreambuf_iterator<char>(), '\n');
     std::string temp;
-    y_size = 0;
-    while(temp_stream >> temp){
-        y_size ++;
-    }
     stream >> temp;
     x_size = temp.size();
+    map = new char*[y_size];
     size_t size = 0;
     do{
-        for(int i = 0; i < x_size; i++){
+        map[size] = new char[x_size];
+        for(size_t i = 0; i < x_size; i++){
             map[size][i] = temp[i];
         }
     }while(stream >> temp);
+}
+
+Map::~Map(){
+    for(size_t i = 0; i < y_size; i++){
+        delete []map[i];
+    }
+    delete []map;
+}
+
+std::string Map::route(Point src, Point dst){
+    return "Stub";
 }
--- labs/bomber/Map.h
+++ labs/bomber/Map.h
@@ -3,17 +3,20 @@
 
 #include <iostream>
 #include <string>
+#include <iterator>
+#include <algorithm>
 
 #include "Point.h"
 
 
 class Map {
     // Member Variables
-    char* map;
+    char** map;
     size_t x_size;
     size_t y_size;
 public:
     Map(std::istream& stream);
+    ~Map();
     // ~Map();
 
     std::string route(Point src, Point dst);
